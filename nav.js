494569a5caf923d1579a9fef530ec41a78813019
Anshul Gupta <hi.anshulg@gmail.com>
2021-01-09 10:13:50 -0800

    resize

--- nav.js
+++ nav.js
@@ -2,6 +2,12 @@ let progress = document.getElementById("scroll");
 let p = document.getElementById("percent");
 let max = document.body.scrollHeight - window.innerHeight;
 let change = (window.pageYOffset / max) * 100;
+
+window.onresize = function (event) {
+  max = document.body.scrollHeight - window.innerHeight;
+  change = (window.pageYOffset / max) * 100;
+};
+
 console.log(change);
 if (change == 0) {
   p.innerHTML = "Page Scrolled " + Math.round(change) + "%";
