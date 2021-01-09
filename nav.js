2a21487a9bf31c19a6ad302acf1b742870a551c6
Anshul Gupta <hi.anshulg@gmail.com>
2021-01-09 12:26:36 -0800

    clean

--- nav.js
+++ nav.js
@@ -19,3 +19,25 @@ window.onscroll = function () {
   //   p.innerHTML = "Page Scrolled " + Math.round(change) + "%";
   p.innerHTML = Math.round(change) + "%";
 };
+
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark",
+};
+
+const theme =
+  localStorage.getItem("theme") ||
+  ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem("theme");
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem("theme", next);
+}
+
+document.getElementById("themeButton").onclick = toggleTheme;
